dee88d7b27f76199c6dd5841f8b3ff3391efda3e
Zhaofeng Li <hello@zhaofeng.li>
2021-09-13 02:16:05 -0700

    Rename to realjump, add more stuff

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
-# mbrjump
+# realjump
 
-`mbrjump` is a tiny `#![no_std]` crate that boots a supplied MBR header from your x86-64 kernel.
-It puts the system from Long Mode back to Real Mode and jumps to the MBR.
+`realjump` is a tiny `#![no_std]` crate that loads and boots Real Mode code (MBR, GRUB2, etc.) from your x86-64 kernel.
+It puts the system from Long Mode back to Real Mode and jumps to the code.
+
+## But why?
 
 It's mostly useful for quickly getting back to Linux from your OS because the boot process can be very slow on server hardware, and you are too lazy to implement some kexec-like functionality in your kernel.
 
 ## Usage
 
-1. Load the MBR from somewhere
-1. Disable interrupts
-1. Identity map the first 1MiB of memory as executable
-1. Call `mbrjump::boot_mbr(your_mbr_slice).unwrap()`
-1. ???
-1. Profit!
+1. Disable interrupts.
+1. Identity map the first 1MiB of memory as executable.
+1. Halt all other processors.
+1. Call `realjump::boot(your_mbr_slice, 0x7c00).unwrap()`.
+1. `realjump` will take care of the rest. It will load its own GDT and take the system all the way back to Real Mode.
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,39 @@
+//! Tiny no_std crate that loads and boots Real Mode code from an x86-64 kernel.
+//!
+//! `realjump` is a tiny `#![no_std]` crate that loads and boots Real Mode code (MBR, GRUB2, etc.) from your x86-64 kernel.
+//! It puts the system from Long Mode back to Real Mode and jumps to the code.
+
 #![no_std]
 
 #![feature(asm)]
+#![deny(missing_docs)]
 
 use core::result::Result;
-use core::ptr::copy;
+use core::ptr::{copy, write_volatile};
+
+/// The maximum payload size.
+pub const MAX_PAYLOAD_SIZE: usize = 0x78000;
+
+/// Size of the bootstrap code.
+pub const BOOTSTRAP_CODE_SIZE: usize = include_bytes!(concat!(env!("OUT_DIR"), "/redpill.bin")).len();
+
+/// The bootstrap code.
+static BOOTSTRAP_CODE: &'static [u8] = include_bytes!(concat!(env!("OUT_DIR"), "/redpill.bin"));
+
+/// Where the bootstrap code should be copied to.
+const BOOTSTRAP_CODE_DEST: u16 = 0x500;
 
 /// An error.
 #[derive(Debug)]
 pub enum Error {
+    /// The payload is too large.
+    PayloadTooLarge,
+
+    /// The payload would overlap with the bootstrap code.
+    PayloadWouldOverlap,
+
     /// The MBR magic is invalid.
-    InvalidMagic,
+    InvalidMbrMagic,
 }
 
 /// Boot an MBR.
@@ -20,18 +44,46 @@ pub enum Error {
 /// - Disable interrupts
 pub unsafe fn boot_mbr(mbr: &[u8; 512]) -> Result<(), Error> {
     if mbr[510] != 0x55 || mbr[511] != 0xaa {
-        return Err(Error::InvalidMagic);
+        return Err(Error::InvalidMbrMagic);
     }
 
-    let code_dest = 0x500 as *mut u8;
-    let mbr_dest = 0x7c00 as *mut u8;
+    boot(mbr, 0x7c00)
+}
 
-    let code = include_bytes!(concat!(env!("OUT_DIR"), "/redpill.bin"));
+/// Boot a GRUB2 core image.
+pub unsafe fn boot_grub2(image: &[u8]) -> Result<(), Error> {
+    boot_offset(image, 0x8000, 0x200)
+}
 
-    copy(mbr as *const [u8] as _, mbr_dest, mbr.len());
-    copy(code as _, code_dest, code.len());
+/// Copy a payload to a destination, then jump to it.
+///
+/// This is equivalent to calling `boot_offset(payload, destination, 0)`.
+pub unsafe fn boot(payload: &[u8], destination: u16) -> Result<(), Error> {
+    boot_offset(payload, destination, 0)
+}
 
-    asm!("mov rax, 0x500", "jmp rax");
+/// Copy a payload to a destination, then jump to an offset.
+///
+/// For a GRUB2 `core.img`, the destination and offset should be 0x8000 and 0x200 respectively.
+pub unsafe fn boot_offset(payload: &[u8], destination: u16, offset: u16) -> Result<(), Error> {
+    if payload.len() > MAX_PAYLOAD_SIZE {
+        return Err(Error::PayloadTooLarge);
+    }
+
+    let p = (destination, destination + offset);
+    let b = (BOOTSTRAP_CODE_DEST, BOOTSTRAP_CODE_DEST + BOOTSTRAP_CODE_SIZE as u16);
+
+    if p.0 <= b.1 && b.0 <= p.1 {
+        return Err(Error::PayloadWouldOverlap);
+    }
+
+    copy(payload as *const [u8] as _, destination as *mut u8, payload.len());
+    copy(BOOTSTRAP_CODE as *const [u8] as _, BOOTSTRAP_CODE_DEST as *mut u8, BOOTSTRAP_CODE.len());
+
+    let entrypoint = (destination + offset) as u16;
+    write_volatile(BOOTSTRAP_CODE_DEST as *mut u16, entrypoint);
+
+    asm!("jmp rax", in("rax") BOOTSTRAP_CODE_DEST + 2);
 
     unreachable!()
 }
