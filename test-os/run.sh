b489a7cda40b39ce5f4e77a7ce7b754ca87b2b52
Zhaofeng Li <hello@zhaofeng.li>
2021-09-14 10:25:27 -0700

    test-os: Add a 30s timeout

--- test-os/run.sh
+++ test-os/run.sh
@@ -8,9 +8,13 @@ cargo build
 objcopy -O elf32-i386 target/x86_64-unknown-none/debug/fakeos fakeos.32
 
 set +e
-qemu-system-x86_64 -kernel fakeos.32 -nographic -device isa-debug-exit,iobase=0xf4,iosize=0x4
+timeout --foreground 30 qemu-system-x86_64 -kernel fakeos.32 -nographic -device isa-debug-exit,iobase=0xf4,iosize=0x4
 
 code=$?
+
+if [ $code -eq 124 ]; then
+	echo "Timeout: test-os/src/mbr.S was not executed in time"
+fi
 
 if [ $code -ne 33 ]; then
 	echo "QEMU exited with $code - should be 33"
